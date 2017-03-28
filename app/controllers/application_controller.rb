baa52426108e0efd351ae3e93aec90a486abf2c0
高原颯起 <st.24441121@gmail.com>
2017-03-28 12:35:18 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     def after_sign_out_path_for(resource)
     '/users/sign_in'
-  end
+    end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
