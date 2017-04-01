0eeaf071f1cded83d3832ff5ef4e7b4b21aadaec
高原颯起 <st.24441121@gmail.com>
2017-04-02 01:00:50 +0900

    Add update_params To users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
-  user = User.find(params[:id]) 
+
+   def
+   @user = User.find(params[:id]) 
+   end
 
    def edit
    end
 
    def update
+    current_user.update(update_params)
+   end
+
+   private
+   def update_params
+    params.require(:user).permit(:name)
    end
 end
