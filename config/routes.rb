c4be316ac02a24f8590d0e12b34ddbcf0f2362ed
高原颯起 <st.24441121@gmail.com>
2017-04-22 01:31:33 +0900

    Add LInk to "Edit"&fa-pencil icon

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   root 'groups#index'
-  get groups/new : 'groups#new'
-  post groups : 'groups#create'
+  resources :groups
+  # get groups/new: "groups#new"
+  # post groups: "groups#create"
 end
