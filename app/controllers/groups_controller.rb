cc0da19bd5b0e6420577a817f71ef6f103099661
高原颯起 <st.24441121@gmail.com>
2017-04-22 00:15:50 +0900

    Add Create Action To groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,9 +4,22 @@ class GroupsController < ApplicationController
   def index
   end
 
+  def new
+    @group = Group.new
+  end
 
+  def create
+    Group.create(name: group_params[:name])
+    redirect_to action: :index 
+  end
+  
+  private
+
+   def group_params
+     params.require(:chat_group).permit(:name)
+   end
 
    def move_to_index
-       redirect_to action: :index unless user_signed_in?
+     redirect_to action: :index unless user_signed_in?
    end
 end
