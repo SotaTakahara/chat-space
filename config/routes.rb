04e2f2f5be9e3bab9629b69815f34e913f3ab044
SotaTakahara <st.24441121@gmail.com>
2017-05-04 18:29:52 +0900

    Revert "Implement_Edit_Group_Function"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   root 'groups#index'
-  resources :groups do 
-    resources :messages 
-  end 
+  get groups/new : 'groups#new'
+  post groups : 'groups#create'
 end
