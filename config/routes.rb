267e922d564fbc73e7653136542ce8bf436340e3
高原颯起 <st.24441121@gmail.com>
2017-05-04 10:19:24 +0900

    Make nets of routing of messag

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   root 'groups#index'
-  resources :groups
-  # get groups/new: "groups#new"
-  # post groups: "groups#create"
+  resources :groups do 
+    resources :messages 
+  end 
 end
