106626658d514fa099b0bc8fff056afa84637592
高原颯起 <st.24441121@gmail.com>
2017-05-27 09:06:56 +0900

    Modify how to write validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to :user  
-  belongs_to :group  
-  validate :text, :user_id, :group_id
+  belongs_to :user
+  belongs_to :group
+  validates :text, :user_id, :group_id, presence: true
 end
