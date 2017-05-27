9e2569394843371addd5cb36434a7f4dc9bedb26
高原颯起 <st.24441121@gmail.com>
2017-05-27 09:05:06 +0900

    Make foreign key reference type

--- db/migrate/20170516152013_add_columns_to_messages.rb
+++ db/migrate/20170516152013_add_columns_to_messages.rb
@@ -1,7 +1,7 @@
 class AddColumnsToMessages < ActiveRecord::Migration[5.0]
   def change
+    add_reference :messages, :user
+    add_reference :messages, :group
     add_column :messages, :text, :string
-    add_column :messages, :group_id, :integer
-    add_column :messages, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,13 @@ ActiveRecord::Schema.define(version: 20170516152013) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "text"
-    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "group_id"
+    t.string   "text"
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
